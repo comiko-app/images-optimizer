7fcea56c890275e128133c7188fd38727b97e62e
Joel Trottier-Hebert <iamquebecois@gmail.com>
2018-02-05 20:56:08 -0500

    Removed generated js file

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,8 +2,8 @@
 exports.__esModule = true;
 var functions = require("firebase-functions");
 var axios_1 = require("axios");
-var urlOfImage = "https://firebasestorage.googleapis.com/v0/b/comiko-3916d.appspot.com/o/photos%2FGuillaume%20Wagner.jpg?alt=media&token=aac4acaf-6b27-4f19-8451-3901d2990578";
-var key = "8QvKLKSn67Llb3wcGzAWYzG0Y2-uLh3c";
+var cors = require('cors')({ origin: true });
+var key = functions.config().api.key;
 function compressImage(url) {
     var options = {
         url: '/shrink',
@@ -32,15 +32,11 @@ function compressMultipleImages(images) {
         };
     })["catch"](function (e) { return console.log(e); }); }));
 }
-var sampleImages = [
-    { 'id': '12312', 'url': urlOfImage },
-    { 'id': '12313', 'url': urlOfImage }
-];
-compressMultipleImages(sampleImages).then(function (data) { return console.log(data); });
 exports.optimizeImages = functions.https.onRequest(function (request, response) {
-    if (request.method === "POST") {
-        var images = request.body;
-        console.log("Some logging! " + JSON.stringify(images));
-        compressMultipleImages(images).then(function (data) { return response.json(data); });
-    }
+    cors(request, response, function () {
+        if (request.method === 'POST') {
+            var images = request.body;
+            compressMultipleImages(images).then(function (data) { return response.json(data); });
+        }
+    });
 });
